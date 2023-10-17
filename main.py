511bad04d15114955cc60045b50c30d9e7ab7d4f
Viktor Shishkalov <viktorshish@gmail.com>
2023-10-17 16:15:25 +0300

    Сравнение количества вакансий

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 
 
-payload = {'professional_role': '96'}
+payload = {'professional_role': '96', 'area': '1', 'period': 30}
 response = requests.get('https://api.hh.ru/vacancies', params=payload)
-monthly_vacancies = response.json()
-print(monthly_vacancies)
+monthly_moscow_vacancies_found = response.json()['found']
+print('Найдено вакансий за месяц в Москве: ', monthly_moscow_vacancies_found)
+
+payload = {'professional_role': '96', 'area': '1'}
+response = requests.get('https://api.hh.ru/vacancies', params=payload)
+moscow_vacancies_found = response.json()['found']
+print('Найдено вакансий за все время в Москве: ', monthly_moscow_vacancies_found)
