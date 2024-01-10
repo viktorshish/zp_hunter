24b6aa911083116eeebce1cf18eb28e019578abe
Viktor Shishkalov Think <viktorshish@gmail.com>
2024-01-11 02:46:47 +0300

    fixed calculation of vacancies by period

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
 
 
-payload = {'professional_role': '96', 'area': '1', 'period': 30}
-response = requests.get('https://api.hh.ru/vacancies', params=payload)
-monthly_moscow_vacancies_found = response.json()['found']
-print('Найдено вакансий за месяц в Москве: ', monthly_moscow_vacancies_found)
+def get_month_vacancy(period=None):
+    payload = {'professional_role': '96', 'area': '1'}
+    if period is not None:
+        payload['period'] = period
+    response = requests.get('https://api.hh.ru/vacancies', params=payload)
+    response.raise_for_status()
+    print(response.json())
+    return response.json()['found']
 
-payload = {'professional_role': '96', 'area': '1'}
-response = requests.get('https://api.hh.ru/vacancies', params=payload)
-moscow_vacancies_found = response.json()['found']
-print('Найдено вакансий за все время в Москве: ', monthly_moscow_vacancies_found)
+
+if __name__ == '__main__':
+    periods = [30, None]
+    for period in periods:
+        if period:
+            print(f'Число вакансий за последний месяц в Москве: {get_month_vacancy(period)}')
+        else:
+            print(f'Число вакансий за все время в Москве: {get_month_vacancy()}')
