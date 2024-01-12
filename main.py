8d4a8fade53b2bc6f27d157eaf483b3aaea4206e
Viktor Shishkalov Think <viktorshish@gmail.com>
2024-01-12 03:13:06 +0300

    Add Language Comparison

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ def get_month_vacancy(period=None):
         payload['period'] = period
     response = requests.get('https://api.hh.ru/vacancies', params=payload)
     response.raise_for_status()
-    print(response.json())
+    print(response.text)
+
+    # with open('vacansy.txt', 'w', encoding='utf-8') as file:
+    #     file.write(response.text)
+
+    languages = ['Python', 'C#', 'C', 'C++', 'Java', 'Javascript']
+    languages_count = {}
+    for language in languages:
+        count = 0
+        for vacancy in response.json()['items']:
+            if language in vacancy['name']:
+                count+=1
+        languages_count[language] = count
+    print(languages_count)
     return response.json()['found']
 
 
