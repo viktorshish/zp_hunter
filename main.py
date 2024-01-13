b3599bac304d2db4b5ad3bea8a354a5e0cce74f6
Viktor Shishkalov <viktorshish@gmail.com>
2024-01-13 12:45:38 +0300

    Fix get vacancy

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
+import json
+
 import requests
 
 
-def get_month_vacancy(period=None):
-    payload = {'professional_role': '96', 'area': '1'}
+def get_vacancy(period=None):
+    payload = {'professional_role': '96', 'area': '1', 'per_page': '100'}
     if period is not None:
         payload['period'] = period
     response = requests.get('https://api.hh.ru/vacancies', params=payload)
     response.raise_for_status()
-    print(response.text)
+    return response.json()
 
-    # with open('vacansy.txt', 'w', encoding='utf-8') as file:
-    #     file.write(response.text)
 
-    languages = ['Python', 'C#', 'C', 'C++', 'Java', 'Javascript']
+def save_in_txt_file():
+    vacancies = get_vacancy()
+    with open('vacansys.txt', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(vacancies, ensure_ascii=False, indent=4))
+
+
+def compare_languages():
+    languages = ['Python', 'C#', 'C', 'C++', 'Java', 'Javascript', 'PHP', 'Ruby', 'Go', 'TypeScript']
     languages_count = {}
     for language in languages:
         count = 0
-        for vacancy in response.json()['items']:
+        for vacancy in get_vacancy()['items']:
             if language in vacancy['name']:
                 count+=1
         languages_count[language] = count
     print(languages_count)
-    return response.json()['found']
 
 
-if __name__ == '__main__':
+def main():
     periods = [30, None]
     for period in periods:
         if period:
-            print(f'Число вакансий за последний месяц в Москве: {get_month_vacancy(period)}')
+            print(f'Число вакансий за последний месяц в Москве: {get_vacancy(period)["found"]}')
         else:
-            print(f'Число вакансий за все время в Москве: {get_month_vacancy()}')
+            print(f'Число вакансий за все время в Москве: {get_vacancy()["found"]}')
+    save_in_txt_file()
+    compare_languages()
+
+
+if __name__ == '__main__':
+    main()
