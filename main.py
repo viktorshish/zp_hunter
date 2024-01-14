1623672246955ef834bbabd80e86e7dd6eaa20f8
Viktor Shishkalov <viktorshish@gmail.com>
2024-01-14 22:22:16 +0300

    Write function get_salary_from_a_language_vacancy

--- main.py
+++ main.py
@@ -1,59 +1,63 @@
-import json
-
 import requests
 
 
-def get_vacancy(period=None):
+def get_vacancies():
     payload = {'professional_role': '96', 'area': '1', 'per_page': '100'}
-    if period is not None:
-        payload['period'] = period
     response = requests.get('https://api.hh.ru/vacancies', params=payload)
     response.raise_for_status()
     return response.json()
 
 
-def save_in_txt_file(vacancies):
-    with open('vacansys.txt', 'w', encoding='utf-8') as file:
-        file.write(json.dumps(vacancies, ensure_ascii=False, indent=4))
-
-
-def comparing_by_period():
-    periods = [30, None]
-    for period in periods:
-        if period:
-            print(f'Число вакансий за последний месяц в Москве: {get_vacancy(period)["found"]}')
-        else:
-            print(f'Число вакансий за все время в Москве: {get_vacancy()["found"]}')
-
-
 def compare_languages(vacancies):
-    languages = ['Python', 'C#', 'C', 'C++', 'Java', 'Javascript', 'PHP', 'Ruby', 'Go', 'TypeScript']
+    languages = ['Python', 'C#', 'C++', 'Java', 'Javascript', 'PHP', 'Ruby', 'Go', 'TypeScript']
     languages_count = {}
     for language in languages:
         count = 0
         for vacancy in vacancies['items']:
-            if language in (vacancy['name'] or vacancy['snippet']['requirement'] or vacancy['snippet']['responsibility']):
+            if language in vacancy['name']:
+                count += 1
+            elif (vacancy['snippet'].get('requirement') is not None
+                  and language in vacancy['snippet']['requirement']):
+                count += 1
+            elif (vacancy['snippet'].get('responsibility') is not None
+                  and language in vacancy['snippet']['responsibility']):
                 count += 1
         languages_count[language] = count
     print(languages_count)
 
 
-def get_salaries_by_vacancies(vacancies):
+def get_salary_from_a_language_vacancy(vacancy, language):
+    if language in vacancy['name']:
+        return f"{vacancy['salary']} : {vacancy['name']}"
+    elif (vacancy['snippet'].get('requirement') is not None
+          and language in vacancy['snippet']['requirement']):
+        return f"{vacancy['salary']} : {vacancy['name']} -  {vacancy['snippet']['requirement']}"
+    elif (vacancy['snippet'].get('responsibility') is not None
+          and language in vacancy['snippet']['responsibility']):
+        return f"{vacancy['salary']} : {vacancy['name']} - {vacancy['snippet']['responsibility']}"
+
+
+# def predict_rub_salary(salary):
+#     if salary and 'RUR' in salary['currency']:
+#         if salary.get('from') and salary.get('to'):
+#             return (int(salary['from']) + int(salary['to'])) / 2
+#         elif salary.get('from'):
+#             return int(salary['from']) * 1.2
+#         elif salary.get('to'):
+#             return int(salary['to']) * 0.8
+
+
+def predict_rub_salary_by_language(vacancies):
+    language = 'Python'
     for vacancy in vacancies['items']:
-        print(vacancy)
-        if 'Python' in (vacancy['name'] or vacancy['snippet']['requirement'] or vacancy['snippet']['responsibility']):
-            print(vacancy['salary'])
+        print(get_salary_from_a_language_vacancy(vacancy, language))
 
 
 def main():
-    comparing_by_period()
-
-    vacancies = get_vacancy()
-    save_in_txt_file(vacancies)
-
+    vacancies = get_vacancies()
     compare_languages(vacancies)
 
-    get_salaries_by_vacancies(vacancies)
+    predict_rub_salary_by_language(vacancies)
 
 
 if __name__ == '__main__':
