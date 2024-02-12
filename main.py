dda4731a0ec528dcd7e10f20cd054ae2adba9867
Viktor Shishkalov <viktorshish@gmail.com>
2024-02-12 03:16:18 +0300

    Combined functions of average salary calculation

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def get_vacancies_hh(language):
             'area': ID_HH_MOSCOW,
             'text': language,
             'page': page,
-            'per_page': vacancies_on_the_page
+            'per_page': vacancies_on_the_page,
+            'currency': 'RUR'
         }
         url = 'https://api.hh.ru/vacancies'
 
@@ -34,28 +35,38 @@ def get_vacancies_hh(language):
         if page+1 >= page_response.json()['pages']:
             break
         page += 1
-        time.sleep(1)
+        time.sleep(5)
     return vacancies
 
 
-def predict_rub_salary_hh(salary):
-    if salary and ('RUR' in salary['currency']):
-        if salary.get('from') and salary.get('to'):
-            return (int(salary['from']) + int(salary['to'])) / 2
-        elif salary.get('from'):
-            return int(salary['from']) * 1.2
-        elif salary.get('to'):
-            return int(salary['to']) * 0.8
+def predict_rub_salary(vacancy):
+    if vacancy.get('salary'):
+        from_salary = vacancy['salary'].get('from')
+        to_salary = vacancy['salary'].get('to')
+    elif vacancy.get('currency'):
+        from_salary = vacancy.get('payment_from')
+        to_salary = vacancy['payment_to']
+    else:
+        return None
+
+    if from_salary and to_salary:
+        return (int(from_salary) + int(to_salary)) / 2
+    elif from_salary:
+        return int(from_salary) * 1.2
+    elif to_salary:
+        return int(to_salary) * 0.8
+    else:
+        return None
 
 
-def calculate_the_average_salary_for_language_hh(vacancies):
+def calculate_the_average_salary_for_language(vacancies):
     count_vacancies_with_salary = 0
     amount_salary = 0
     for vacancy in vacancies:
-        predicted_salary = predict_rub_salary_hh(vacancy['salary'])
+        predicted_salary = predict_rub_salary(vacancy)
 
         if predicted_salary is not None:
-            amount_salary += predict_rub_salary_hh(vacancy['salary'])
+            amount_salary += predict_rub_salary(vacancy)
             count_vacancies_with_salary += 1
 
     try:
@@ -93,37 +104,6 @@ def get_vacancies_sj(language, sj_key):
     return vacancies
 
 
-def predict_rub_salary_sj(vacancy):
-    if 'rub' in vacancy['currency']:
-        if vacancy.get('payment_from') and vacancy.get('payment_to'):
-            return (int(vacancy['payment_from']) + int(vacancy['payment_to'])) / 2
-        elif vacancy.get('payment_from'):
-            return int(vacancy['payment_from']) * 1.2
-        elif vacancy.get('payment_to'):
-            return int(vacancy['payment_to']) * 0.8
-
-
-def calculate_the_average_salary_by_language_sj(vacancies):
-    count_vacancies_with_salary = 0
-    amount_salary = 0
-    for vacancy in vacancies:
-        predicted_salary = predict_rub_salary_sj(vacancy)
-
-        if predicted_salary is not None:
-            amount_salary += predict_rub_salary_sj(vacancy)
-            count_vacancies_with_salary += 1
-
-    try:
-        average = int(amount_salary / count_vacancies_with_salary)
-    except ZeroDivisionError:
-        average = 0
-    return {
-        'vacancies_found': len(vacancies),
-        'vacancies_processed': count_vacancies_with_salary,
-        'average_salary': average
-    }
-
-
 def convert_statistics_to_table(statistics, title):
     final_statistic = [
         ['Язык программирования', 'Вакансий найдено',
@@ -152,11 +132,11 @@ def main():
     comparison_of_languages_by_vacancies_sj = {}
     for language in LANGUAGES:
         vacancies_hh = get_vacancies_hh(language)
-        average_salaries_hh = calculate_the_average_salary_for_language_hh(vacancies_hh)
+        average_salaries_hh = calculate_the_average_salary_for_language(vacancies_hh)
         comparison_of_languages_by_vacancies_hh[language] = average_salaries_hh
 
         vacancies_sj = get_vacancies_sj(language, sj_key)
-        average_salaries_sj = calculate_the_average_salary_by_language_sj(vacancies_sj)
+        average_salaries_sj = calculate_the_average_salary_for_language(vacancies_sj)
         comparison_of_languages_by_vacancies_sj[language] = average_salaries_sj
 
     print(convert_statistics_to_table(comparison_of_languages_by_vacancies_hh, 'HeadHunter Moscow'))
